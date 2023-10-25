71be11aa67c3775d3855270d2218963f038fe61c
hemengke1997 <23536175@qq.com>
2023-10-25 19:39:50 +0800

    fix: ci

--- virtual.d.ts
+++ virtual.d.ts
@@ -1,3 +1,5 @@
 declare module 'virtual:i18n-helper' {
-  export default Record<string, () => Promise<{ default: Record<string, any> | undefined }>>
+  type I18nHelper = Record<string, () => Promise<{ default: Record<string, any> | undefined }>>
+  const i18nHelper: I18nHelper
+  export default i18nHelper
 }
